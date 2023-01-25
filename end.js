7844070c939edc6352566d94908f747c34779f7b
uhkz <115628083+uhkz@users.noreply.github.com>
2023-01-25 14:23:55 +0100

    Werkende versie
    + quiz 1

--- end.js
+++ end.js
@@ -1,13 +1,13 @@
-const username = document.querySelectorAll('#username')
-const saveScoreBtn = document.querySelectorAll('#saveScoreBtn')
-const finalScore = document.querySelectorAll('#finalScore')
+const username = document.querySelector('#username')
+const saveScoreBtn = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 const MAX_HIGH_SCORES = 5
 
-finalScore.innexText = mostRecentScore
+finalScore.innerText = mostRecentScore
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value
@@ -17,18 +17,20 @@ saveHighScore = e => {
     e.preventDefault()
 
     const score = {
-        score: mostRecentScore, 
-        name: username.value 
+        score: mostRecentScore,
+        name: username.value
     }
 
     highScores.push(score)
 
-    highScores.sort((a,b) =>{
+    highScores.sort((a,b) => {
         return b.score - a.score
     })
 
     highScores.splice(5)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/')
+    window.location.assign('')
+
+    
 }
